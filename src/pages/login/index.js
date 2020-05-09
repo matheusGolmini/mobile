baff185b02245c998b5017cbaafefa335dcc4d74
Matheus Fernando <matheus.golmini@wisereducacao.com>
2020-05-09 11:26:20 -0300

    feat: login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 
-import logoImg from '../../assets/test.jpeg';
+import logoImg from '../../assets/logo.png';
 import styles from './styles'; 
 
+import api from '../../services/api';
+
 export default function Login(){
+    const [password, setPassword] = useState('');
+    const[email, setEmail] = useState('');
+
+    login = async () => {
+        const data = ({
+            email,
+            password
+        })
+        try {
+            const response = await api.post('login', data);
+            console.log(response.data);
+            Alert.alert("Será que foi?", `${response.data}`)
+        } catch (error) {
+            alert('Deu ruim')
+        }
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-                <Image source={logoImg}/>
+                <Image style={styles.logo} source={logoImg}/>
                 <Text style={ styles.headerText }>Seja bem vindo!</Text>
             </View> 
+            <TextInput 
+                style={styles.input} 
+                // value={email}
+                onChangeText={(val) => setEmail(val)}
+                placeholder='Digite seu E-mail' 
+            />
+            <TextInput 
+                style={styles.input} 
+                secureTextEntry={true} 
+                // value={password}
+                onChangeText={(val) => setPassword(val)}
+                placeholder='Digite sua senha' 
+            />
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={ () => { this.login() }}
+            >
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+
+         
             <Text style={styles.title}>Bem vindo</Text>
             <Text style={styles.description}>Faça login e salve o mundo!!</Text>
         </View>
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -4,32 +4,64 @@ import Constants from 'expo-constants';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20
+        justifyContent: 'center',
+        alignItems: 'center'
+        // paddingHorizontal: 24,
+        // paddingTop: Constants.statusBarHeight + 20
         
     },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
+    // header: {
+    //     flexDirection: 'row',
+    //     justifyContent: 'space-between',
+    //     alignItems: 'center'
+    // },
+    logo: {
+        width: 150,
+        height: 150,
+        borderRadius: 100
+
     },
-    headerText: { 
-        fontSize: 15,
-        color: '#737380'
-    },
-    headerTextBold: { 
-        fontWeight: 'bold'
-    },
-    title: {
-        fontSize: 30,
-        marginBottom: 16,
-        marginTop: 16,
-        color: '#13131a',
-        fontWeight: 'bold'
-    },
-    description: {
+    input: {
+        marginTop: 10,
+        padding: 10,
+        width: 300,
+        height: 40,
+        backgroundColor: '#fff',
+        borderRadius: 15,
         fontSize: 16,
-        lineHeight:24,
-        color: '#737380'
+        fontWeight: 'bold'
+    },
+    button : {
+        width: 300,
+        height: 42,
+        backgroundColor: '#3498db',
+        marginTop: 10,
+        borderRadius: 4,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#fff'
     }
+//     headerText: { 
+//         fontSize: 15,
+//         color: '#737380'
+//     },
+//     headerTextBold: { 
+//         fontWeight: 'bold'
+//     },
+//     title: {
+//         fontSize: 30,
+//         marginBottom: 16,
+//         marginTop: 16,
+//         color: '#13131a',
+//         fontWeight: 'bold'
+//     },
+//     description: {
+//         fontSize: 16,
+//         lineHeight:24,
+//         color: '#737380'
+//     }
 });
