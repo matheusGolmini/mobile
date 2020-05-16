c43f8534349ce62030da6316ffa2e58e17b5db14
Matheus Fernando <matheus.golmini@wisereducacao.com>
2020-05-16 15:39:52 -0300

    feat: add new routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from './pages/login';
 import Register from './pages/register';
+import Home from './pages/home';
+import Tasks from './pages/tasks';
 import Profile from './pages/profile';
 
 const AppStack = createStackNavigator();
@@ -14,7 +16,9 @@ export default function Routes(){
            <AppStack.Navigator screenOptions={{ headerShown: false }}>
                <AppStack.Screen name='Login' component={Login} />
                <AppStack.Screen name="Register" component={Register} />
-               <AppStack.Screen name="Profile" component={Profile} />
+               <AppStack.Screen name="Home" component={Home} />
+               <AppStack.Screen name="Tasks" component={Tasks}/>
+               <AppStack.Screen name="Profile" component={Profile}/>
            </AppStack.Navigator>
        </NavigationContainer>
     )
