9ee52859c72e4ebfe324f43046e3728dbda9ecc1
Matheus Fernando Golmini <matheusgolminif@gmail.com>
2020-05-31 16:37:58 -0300

    feat: styles

--- src/pages/tasks/styles.js
+++ src/pages/tasks/styles.js
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-
+    container: {
+        flex: 1,
+        paddingHorizontal: 24
+    },
     taskList: {
         marginTop: 15,
     },
